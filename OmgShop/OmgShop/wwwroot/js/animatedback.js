8fb5d4b577c189f89353868bb0944ffe6907e080
Maxitros2 <maxim112111@gmail.com>
2020-10-12 11:26:30 +0300

    Adding animate background

---
+++ OmgShop/OmgShop/wwwroot/js/animatedback.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    function createSquare()
+    {
+        const section = document.querySelector('section');
+        const square = document.createElement('span');
+        square.className ="backsq";
+        var size = Math.random()*50;
+        square.style.width = 20+size+"px";
+        square.style.height = 20+size+"px";
+        
+        square.style.top =  Math.random()*innerHeight+"px";
+        square.style.left = Math.random()*innerWidth+"px";
+        section.appendChild(square);
+        setTimeout(() => 
+                   {
+            square.remove();
+        },5000);
+    }
+	setInterval(createSquare,150);
+});
